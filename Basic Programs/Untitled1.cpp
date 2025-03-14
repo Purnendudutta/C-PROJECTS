32d285c6f31f333058ffa98fd1a3076f46956d3d
Purnendu Dutta <purnendudutta521@gmail.com>
2025-03-14 20:25:05 +0530

    .

--- Basic Programs/Untitled1.cpp
+++ Basic Programs/Untitled1.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+/*
 int sum(int a, int b);
 
 int sum(int x, int y){
@@ -12,6 +12,20 @@ int main() {
 	printf("Enter B:");
 	scanf("%d", &b);
 	int s = sum(a,b);
+	printf("SUM = %d",s);
 	
 }
+*/
 
+void calculate(float value);
+
+void calculate(float value) {
+value = (value + (0.18*value));
+printf("Final Price is : %f",value");	
+}
+
+int main() {
+	float value=100.0;
+	calculate(value);
+	printf("Final Price is : %f",value);
+}
