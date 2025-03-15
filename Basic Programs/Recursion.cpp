6ed2aa98b7cbe45b1b80fc924bd64bbe524b4eb6
Purnendu Dutta <purnendudutta521@gmail.com>
2025-03-15 19:55:19 +0530

    15/03/2025

--- Basic Programs/Recursion.cpp
+++ Basic Programs/Recursion.cpp
@@ -63,7 +63,7 @@ float convertTemp(int celcius) {
  	printf("Fahrenheit : %f",far);
  	return 0;
  }*/
- 
+ /*
  int percent(int num1, int num2, int num3) {
  	return(num1+num2+num3)/3;
  }
@@ -75,3 +75,26 @@ float convertTemp(int celcius) {
  	
  	printf("percentage is :%d",percent(num1,num2,num3));
  }
+ */
+ 
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n, i;
+    
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+    
+    printf("Fibonacci Series: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    
+    return 0;
+}
