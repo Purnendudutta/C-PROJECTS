400ccf37eb1e66fb367063f57987de97f93751df
Purnendu Dutta <purnendudutta521@gmail.com>
2025-03-18 21:17:33 +0530

    19/03/2025

--- Basic Programs/pointer.cpp
+++ Basic Programs/pointer.cpp
@@ -47,3 +47,19 @@ int main() {
 	return 0;
 }
 */
+
+#include<stdio.h>
+void dowork(int a, int b, int *sum, int *prod, int *avg);
+int main() {
+	int a=3,b=5;
+	int sum,prod,avg;
+	dowork(a,b,&sum,&prod,&avg);
+	printf("Sum=%d,Product=%d,Average=%d",sum,prod,avg);
+	return 0;
+}
+
+void dowork(int a, int b, int *sum, int *prod, int *avg) {
+	*sum=a+b;
+	*prod=a*b;
+	*avg=(a+b)/2;
+}
