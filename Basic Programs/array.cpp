685db9ce8e16420f40ed2044add6f042c78942c7
Purnendu Dutta <purnendudutta521@gmail.com>
2025-03-22 11:36:14 +0530

    .

---
+++ Basic Programs/array.cpp
@@ -0,0 +1,31 @@
+//		==== ARRAY ====
+/*
+#include<stdio.h>
+int main() {
+	int arr[3],sum;
+	printf("Enter marks 1:");
+	scanf("%d",&arr[0]);
+	printf("Enter marks 2:");
+	scanf("%d",&arr[1]);
+	printf("Enter marks 3:");
+	scanf("%d",&arr[2]);
+	
+	sum = arr[0]+arr[1]+arr[2];
+	printf("total= %d",sum);
+}
+*/
+
+/*
+#include<stdio.h>
+int main() {
+	float price[3],gst,sum,tot;
+	printf("Enter 3 prices\n");
+	scanf("%f",&price[0]);
+	scanf("%f",&price[1]);
+	scanf("%f",&price[2]);
+	tot=(price[0]+price[1]+price[2]);
+	gst = (price[0]+price[1]+price[2])*0.18;
+	sum = price[0]+price[1]+price[2]+gst;
+	printf("Before gst = %.2f\ngst = %.2f\nTotal bill = %.2f",tot,gst,sum);
+}
+*/
